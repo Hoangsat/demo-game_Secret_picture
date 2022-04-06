1ad25e1f86ada21bc6ca3ad10aa0b4a01af57a3e
Hoangsat <hoangsat1@gmail.com>
2022-04-06 10:32:05 +0200

    secret

--- SecretPicture/SecretPicture/SecretPicture.Designer.cs
+++ SecretPicture/SecretPicture/SecretPicture.Designer.cs
@@ -29,22 +29,88 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SecretPicture));
+            this.buttonShowPicture1 = new System.Windows.Forms.Button();
+            this.buttonShowPicture2 = new System.Windows.Forms.Button();
+            this.buttonShowPicture4 = new System.Windows.Forms.Button();
+            this.buttonShowPicture3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // buttonShowPicture1
+            // 
+            this.buttonShowPicture1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonShowPicture1.Location = new System.Drawing.Point(12, 12);
+            this.buttonShowPicture1.Name = "buttonShowPicture1";
+            this.buttonShowPicture1.Size = new System.Drawing.Size(147, 68);
+            this.buttonShowPicture1.TabIndex = 0;
+            this.buttonShowPicture1.Text = "Wolf";
+            this.buttonShowPicture1.UseVisualStyleBackColor = true;
+            this.buttonShowPicture1.Click += new System.EventHandler(this.buttonShowPicture1_Click);
+            // 
+            // buttonShowPicture2
+            // 
+            this.buttonShowPicture2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonShowPicture2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonShowPicture2.Location = new System.Drawing.Point(443, 12);
+            this.buttonShowPicture2.Name = "buttonShowPicture2";
+            this.buttonShowPicture2.Size = new System.Drawing.Size(147, 68);
+            this.buttonShowPicture2.TabIndex = 0;
+            this.buttonShowPicture2.Text = "Raccoon";
+            this.buttonShowPicture2.UseVisualStyleBackColor = true;
+            this.buttonShowPicture2.Click += new System.EventHandler(this.buttonShowPicture2_Click);
+            // 
+            // buttonShowPicture4
+            // 
+            this.buttonShowPicture4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonShowPicture4.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonShowPicture4.Location = new System.Drawing.Point(12, 257);
+            this.buttonShowPicture4.Name = "buttonShowPicture4";
+            this.buttonShowPicture4.Size = new System.Drawing.Size(147, 68);
+            this.buttonShowPicture4.TabIndex = 0;
+            this.buttonShowPicture4.Text = "Cat";
+            this.buttonShowPicture4.UseVisualStyleBackColor = true;
+            this.buttonShowPicture4.Click += new System.EventHandler(this.buttonShowPicture4_Click);
+            // 
+            // buttonShowPicture3
+            // 
+            this.buttonShowPicture3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonShowPicture3.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonShowPicture3.Location = new System.Drawing.Point(443, 257);
+            this.buttonShowPicture3.Name = "buttonShowPicture3";
+            this.buttonShowPicture3.Size = new System.Drawing.Size(147, 68);
+            this.buttonShowPicture3.TabIndex = 0;
+            this.buttonShowPicture3.Text = "Leopard";
+            this.buttonShowPicture3.UseVisualStyleBackColor = true;
+            this.buttonShowPicture3.Click += new System.EventHandler(this.buttonShowPicture3_Click);
+            // 
+            // SecretPicture
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(602, 352);
+            this.Controls.Add(this.buttonShowPicture3);
+            this.Controls.Add(this.buttonShowPicture4);
+            this.Controls.Add(this.buttonShowPicture2);
+            this.Controls.Add(this.buttonShowPicture1);
+            this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.MinimumSize = new System.Drawing.Size(507, 300);
+            this.Name = "SecretPicture";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SecretPicture";
             this.TopMost = true;
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.SecretPicture_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.SecretPicture_MouseMove);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonShowPicture1;
+        private System.Windows.Forms.Button buttonShowPicture2;
+        private System.Windows.Forms.Button buttonShowPicture4;
+        private System.Windows.Forms.Button buttonShowPicture3;
     }
 }
 
--- SecretPicture/SecretPicture/SecretPicture.cs
+++ SecretPicture/SecretPicture/SecretPicture.cs
@@ -16,5 +16,50 @@ namespace SecretPicture
         {
             InitializeComponent();
         }
+        int secret_nr = 0;
+        private void buttonShowPicture1_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.picture1;
+            secret_nr = 0;
+        }   
+        private void buttonShowPicture2_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.picture2;
+            secret_nr = 0;
+        }
+
+        private void buttonShowPicture4_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Properties.Resources.picture4;
+            secret_nr = 0;
+        }
+
+        private void buttonShowPicture3_Click(object sender, EventArgs e)
+        {
+            if (secret_nr == 1)
+                this.BackgroundImage = Properties.Resources.secret;
+            else
+            this.BackgroundImage = Properties.Resources.picture3;
+            secret_nr = 0;
+        }
+
+        private void SecretPicture_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                secret_nr = 1;
+            
+            
+        }
+
+        private void SecretPicture_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(secret_nr==1)
+            if (e.X < buttonShowPicture1.Location.X &&
+                e.Y < buttonShowPicture1.Location.Y)
+                secret_nr = 2;
+                
+            
+
+        }
     }
 }
